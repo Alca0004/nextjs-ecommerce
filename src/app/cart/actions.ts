65152bfbfdd18141758fe864dd3f06b684b5d6b4
alca0004 <alca0004@gmail.com>
2023-12-06 10:04:40 -0500

    Merge anonymous cart

--- src/app/cart/actions.ts
+++ src/app/cart/actions.ts
@@ -4,33 +4,33 @@ import { createCart, getCart } from "@/lib/db/cart";
 import { prisma } from "@/lib/db/prisma";
 import { revalidatePath } from "next/cache";
 
-export async function setProductQuantity(productId:string, quantity: number) {
-    const cart = (await getCart()) ?? await createCart()
+export async function setProductQuantity(productId: string, quantity: number) {
+  const cart = (await getCart()) ?? (await createCart());
 
-    const articleInCart = cart.items.find(items => items.productId === productId)
+  const articleInCart = cart.items.find((item) => item.productId === productId);
 
-    if (quantity === 0) {
-        if (articleInCart) {
-            await prisma.cartItem.delete({
-                where:{id: articleInCart.id}
-            })
-        }
-    } else {
-        if (articleInCart) {
-            await prisma.cartItem.update({
-                where: { id: articleInCart.id },
-                data: { quantity }
-
-            })
-        } else {
-            await prisma.cartItem.create({
-                data: {
-                    CartId: cart.id,
-                    productId,
-                    quantity
-                }
-            })
-        }
+  if (quantity === 0) {
+    if (articleInCart) {
+      await prisma.cartItem.delete({
+        where: { id: articleInCart.id },
+      });
     }
-    revalidatePath("/cart")
+  } else {
+    if (articleInCart) {
+      await prisma.cartItem.update({
+        where: { id: articleInCart.id },
+        data: { quantity },
+      });
+    } else {
+      await prisma.cartItem.create({
+        data: {
+          cartId: cart.id,
+          productId,
+          quantity,
+        },
+      });
+    }
+  }
+
+  revalidatePath("/cart");
 }
